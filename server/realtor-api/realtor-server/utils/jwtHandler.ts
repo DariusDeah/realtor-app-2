2bb0aab1e035208966eaed0501ef257635293711
Darius Deah <Dariusdeah21@gmail.com>
2022-11-10 23:06:14 -0700

    jwt claim name update

--- server/realtor-api/realtor-server/utils/jwtHandler.ts
+++ server/realtor-api/realtor-server/utils/jwtHandler.ts
@@ -5,7 +5,7 @@ export class JWTHandler {
     static signToken(user: User) {
         return jwt.sign(
             {
-                full_name: user.full_name || user.email,
+                name: user.full_name || user.email,
                 email: user.email,
                 photo: user.profile_img_url,
                 state: user.state,
@@ -15,6 +15,7 @@ export class JWTHandler {
                 subject: `pillow|${user.id}`,
                 issuer: 'https://api.pillow-zillow.com',
                 expiresIn: '7d',
+                audience: 'https://pillow-zillow.com',
             },
         );
     }
