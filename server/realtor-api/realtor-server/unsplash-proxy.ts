a44679d60a46a9be2b6655fc7f63dbf150466f2d
Darius Deah <Dariusdeah21@gmail.com>
2022-11-22 21:21:36 -0700

    noted issue with axios, switch to mock data

--- server/realtor-api/realtor-server/unsplash-proxy.ts
+++ server/realtor-api/realtor-server/unsplash-proxy.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { DEFAULT_HEADERS } from './headers';
 import { unsplashTestData } from './mock-unsplash-res';
 import { cacheHelper } from './utils/cacheHelper';
-import { LambdaProxyErrorHandler } from './utils/errorHandler';
+import { LambdaProxyErrorHandler, NotFoundError } from './utils/errorHandler';
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -24,31 +24,33 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent, context: any) =
                 statusCode: 200,
                 body: JSON.stringify({
                     message: 'successful',
-                    data: cachedData,
+                    data: cachedData.data,
                 }),
                 isBase64Encoded: false,
             });
         }
-
-        const res = await axios.get(
-            `https://api.unsplash.com/photos/random?orientation=squarish&count=30&query=house&client_id=A2HoMb_eT7jMZ5sIxHV-b9CkF9_SzYzsh9tvl8lX_28`,
-        );
-
-        console.log(res);
-        const unsplashResponse = res.data;
-        await cacheHelper.addToCache({ id: 'unsplash-cache-data', data: unsplashResponse });
-        response = {
-            headers: { ...DEFAULT_HEADERS },
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'successful ',
-                data: unsplashResponse,
-            }),
-            isBase64Encoded: false,
-        };
+        //issue with axios ?
+        // const res = await axios.get(
+        //     `https://api.unsplash.com/photos/random?orientation=squarish&count=30&query=house&client_id=A2HoMb_eT7jMZ5sIxHV-b9CkF9_SzYzsh9tvl8lX_28`,
+        // );
+        const res = { data: unsplashTestData };
+        if (res.data) {
+            const unsplashResponse = res.data;
+            await cacheHelper.addToCache({ id: 'unsplash-cache-data', data: unsplashResponse });
+            return (response = {
+                headers: { ...DEFAULT_HEADERS },
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'successful ',
+                    data: unsplashResponse,
+                }),
+                isBase64Encoded: false,
+            });
+        } else {
+            return new NotFoundError('unsplash data not found ');
+        }
     } catch (error) {
         console.error(error);
         return new LambdaProxyErrorHandler(error).customResponse();
     }
-    return response;
 };
